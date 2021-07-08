79c070dd3ef412c3d4ca97354a50ffda67b13256
Damian Krata <damiankrata92@tlen.pl>
2021-07-08 15:11:23 +0200

    dictionary

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 #include "md5.h"
+#include <fstream>
 using namespace std;
 
+//     void printAllKLengthRec(char set[], string prefix,
+//                                     int n, int k)
+// {
+     
+//     // Base case: k is 0,
+//     // print prefix
+//     if (k == 0)
+//     {
+//         cout << (prefix) << endl;
+//         return;
+//     }
+ 
+//     // One by one add all characters
+//     // from set and recursively
+//     // call for k equals to k-1
+//     for (int i = 0; i < n; i++)
+//     {
+//         string newPrefix;
+         
+//         // Next character of input added
+//         newPrefix = prefix + set[i];
+         
+//         // k is decreased, because
+//         // we have added a new character
+//         printAllKLengthRec(set, newPrefix, n, k - 1);
+//     }
+ 
+// }
+ 
+// void printAllKLength(char set[], int k,int n)
+// {
+//     printAllKLengthRec(set, "", n, k);
+// }         
+
 int main(int argc, char** argv){
-    unsigned int state[4] = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476};
+    unsigned int state[4] = {0x67452301, 0xefcdab89, 0x98fadcfe, 0x10325476};
     unsigned int precomputed[64] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
                                     0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
                                     0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
@@ -20,7 +55,7 @@ int main(int argc, char** argv){
                                     0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
                                     0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
                                     0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
-    const std::string result = "4f8595ed656b1233f1fdd91fbd692bb7";  
+    /*const std::string result = "4f8595ed656b1233f1fdd91fbd692bb7";  
     std::string hash;
     unsigned int tmp;
     for(int i=0;i<4;i++){
@@ -60,7 +95,31 @@ int main(int argc, char** argv){
                 tmp2 = tmp2 << 1;
             }
         }
-    }              
-    std::cout << "md5 of "<< argv[1] << " "<< md5(argv[1], precomputed, state) << std::endl;
+    }  */
+    
+   
+
+    // cout << "First Test" << endl;
+    // char set1[] = {'R', 'r', 'i', 'I', 's', 'S', 'c', 'C', '-', '5', 'V', 'z', 'Z', '3'};
+    // int k = 6;
+
+    // for(int i=0; i<10 ;i++){
+    //     printAllKLength(set1, i, 14);
+    // }
+    ifstream file;
+    file.open("Polish.dic", ios::in);
+    //const std::string result = "4f8595ed656b1233f1fdd91fbd692bb7"; 
+    const std::string result = "d941946038347b75014587883f41a0ac";
+    std::string line ="";
+    while (!file.eof()){
+        getline(file, line);
+        if(md5(line,precomputed,state)==result){
+            std::cout<<line;
+            break;
+        }
+    }
+    file.close();
+    //std::cout << "md5 of "<< argv[1] << " "<< md5(argv[1], precomputed, state) << std::endl;
+    //std::cout << "md5 of "<< "RISC-5 " << " "<< md5("RISC-5", precomputed, state) << std::endl;
     return 0;
 }
